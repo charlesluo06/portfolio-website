84e10c2f7d52c4018759869e04f07001cb2032c3
charlesluo06 <charlesluo06@gmail.com>
2025-12-30 15:56:28 -0800

    d

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -41,7 +41,7 @@ function About() {
 
             <div className="flex-1 flex flex-col justify-between shadow-md/20 gap-2 bg-white border-gray-200 border px-3 py-3 rounded-2xl hover:ring-2 hover:ring-[#0084ff] transition ease-in-out duration-120 hover:scale-105">
               <img src={GamingIcon} className="w-15" />
-              <p className="font-semibold text-[#0084ff] text-sm">Music</p>
+              <p className="font-semibold text-[#0084ff] text-sm">Gaming</p>
             </div>
           </div>
         </div>
