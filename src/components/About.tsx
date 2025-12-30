be90cf9522a7bd48125dd6ecf4431e6781bf942c
charlesluo06 <charlesluo06@gmail.com>
2025-12-30 00:03:47 -0800

    starting working on about component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,21 @@
+import meSunImg from "../assets/meSun.png"
+
 function About() {
   return (
-    <div className="bg-[#f0f0f0]">
-      <div className="min-h-screen inset-shadow-sm/20 rounded-3xl bg-[#f0f0f0] w-full">
+    <div className="bg-gray-100">
+      <div className="h-[50vh] inset-shadow-sm/20 rounded-4xl bg-gray-100 shadow-md/25 w-full flex items-center p-15 lg:px-60">
+      <div className="w-1/3 lg:w-1/4 flex">
+        <img src={meSunImg}/>
+      </div>
+        <div className="w-3/5 lg:w-1/2 ml-auto text-left">
+          <p className="text-3xl font-semibold mb-15">About</p>
+          <p>
+            Hi! My name is Charles Luo. I’m a second-year Computer Science student at the
+            University of California, Riverside. On my free time, I enjoy
+            listening to music, gaming, and working out. Thank you for
+            stopping by!
+          </p>
+        </div>
       </div>
     </div>
   );
